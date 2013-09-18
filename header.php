3b4babb969aeb187956fc2cd9504d7ecb390d317
tflemings <umadbro554@yahoo.com>
2013-09-18 00:15:14 -0400

    Added initial files

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
+        <title>Bible Community Church</title>
+    </head>
+    <body>
+        
+    
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include 'header.php';
+?>
+
+    <form id="nav">
+        <button type="submit">Home</button>
+        <button type="submit">About</button>
+        <button type="submit">Ministries</button>
+        <button type="submit">Services</button>
+        <button type="submit">Contact</button>
+        
+    </form>
+
+<div id="mid">
+    
+</div>
+<?php
+include 'footer.php';
+?>
